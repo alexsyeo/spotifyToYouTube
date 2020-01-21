fe96c276a3f40f50457db89cfc150f4ded47d287
Alex Yeo <alexsyeo@berkeley.edu>
2020-01-20 19:23:20 -0800

    modifying redirect_uri

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const loginMessage = document.getElementById('login-message')
 
 document.getElementById('spotify-login').addEventListener('click', (e) => {
     const spotify_auth_url = 'https://accounts.spotify.com/authorize'
-    const redirect_uri = 'http://localhost:58249'
+    // const redirect_uri = 'http://localhost:58249'
+    const redirect_uri = 'https://alexsyeo.github.io/spotifyToYouTube/'
     const response_type = 'token'
     const scope = 'playlist-read-private'
     location.assign(`${spotify_auth_url}?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=${response_type}&scope=${scope}`)
