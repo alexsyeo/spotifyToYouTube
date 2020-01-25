46791e2830a7aa513fe80e044b1fc8d7788aef36
Alex Yeo <alexsyeo@berkeley.edu>
2020-01-24 20:55:42 -0800

    removed revoke access button

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const CLIENT_ID = '5276504902-vqgh3qs5ns4hadnjc691go947qbbcqkf.apps.googleusercontent.com'
 const API_KEY = 'AIzaSyDVCNrT1b4C1QlmG598XEop-tHbTyMen9c'
-const SCOPE = 'https://www.googleapis.com/auth/youtube.force-ssl';
+const SCOPE = 'https://www.googleapis.com/auth/youtube.force-ssl'
 const displayMessage = document.getElementById('display-message')
 const convertButton = document.getElementById('convert-button')
 const spotifyLogin = document.getElementById('spotify-login')
@@ -11,13 +11,13 @@ let GoogleAuth
 function handleClientLoad() {
     // Load the API's client and auth2 modules.
     // Call the initClient function after the modules load.
-    gapi.load('client:auth2', initClient);
+    gapi.load('client:auth2', initClient)
 }
 
 function initClient() {
     // Retrieve the discovery document for version 3 of YouTube Data API.
     // In practice, your app can retrieve one or more discovery documents.
-    const discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest';
+    const discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
 
     // Initialize the gapi.client object, which app uses to make API requests.
     // Get API key and client ID from API Console.
@@ -28,58 +28,47 @@ function initClient() {
         'clientId': CLIENT_ID,
         'scope': SCOPE
     }).then(function () {
-        GoogleAuth = gapi.auth2.getAuthInstance();
+        GoogleAuth = gapi.auth2.getAuthInstance()
 
         // Listen for sign-in state changes.
-        GoogleAuth.isSignedIn.listen(updateSigninStatus);
+        GoogleAuth.isSignedIn.listen(setSigninStatus)
 
         // Handle initial sign-in state. (Determine if user is already signed in.)
-        const user = GoogleAuth.currentUser.get();
-        setSigninStatus();
+        const user = GoogleAuth.currentUser.get()
+        setSigninStatus()
 
         // Call handleAuthClick function when user clicks on
         //      "Sign In/Authorize" button.
         $('#sign-in-or-out-button').click(function () {
-            handleAuthClick();
-        });
-        $('#revoke-access-button').click(function () {
-            revokeAccess();
-        });
-    });
+            handleAuthClick()
+        })
+    })
 }
 
 function handleAuthClick() {
     if (GoogleAuth.isSignedIn.get()) {
         // User is authorized and has clicked 'Sign out' button.
-        GoogleAuth.signOut();
+        GoogleAuth.signOut()
     } else {
         // User is not signed in. Start Google auth flow.
-        GoogleAuth.signIn();
+        GoogleAuth.signIn()
     }
-}
-
-function revokeAccess() {
-    GoogleAuth.disconnect();
 }
 
 function setSigninStatus() {
     const user = GoogleAuth.currentUser.get();
     const isAuthorized = user.hasGrantedScopes(SCOPE);
     if (isAuthorized) {
-        $('#sign-in-or-out-button').html('Sign out');
-        $('#revoke-access-button').css('display', 'inline-block');
+        $('#sign-in-or-out-button').html('Sign out')
+        $('#revoke-access-button').css('display', 'inline-block')
         $('#auth-status').html('You are currently signed in and have granted ' +
-            'access to this app.');
+            'access to this app.')
     } else {
-        $('#sign-in-or-out-button').html('Sign In/Authorize');
-        $('#revoke-access-button').css('display', 'none');
+        $('#sign-in-or-out-button').html('Sign In/Authorize')
+        $('#revoke-access-button').css('display', 'none')
         $('#auth-status').html('You have not authorized this app or you are ' +
-            'signed out.');
+            'signed out.')
     }
-}
-
-function updateSigninStatus() {
-    setSigninStatus();
 }
 
 spotifyLogin.addEventListener('click', (e) => {
