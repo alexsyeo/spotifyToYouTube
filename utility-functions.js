39a9cd4d20bc92619f4f9e7ce52abd2166b0c78e
Alex Yeo <alexsyeo@berkeley.edu>
2020-01-27 15:28:49 -0800

    test

--- utility-functions.js
+++ utility-functions.js
@@ -16,15 +16,14 @@ const renderSpotifyPlaylists = (playlists) => {
             }
         })
 
-        listEl.appendChild(checkBox)
-        listEl.textContent = playlist.name
+        // listEl.appendChild(checkBox)
+        // listEl.textContent = playlist.name
         
 
-        // const labelEl = document.createElement('label')
-        // labelEl.appendChild(checkBox)
-        // labelEl.textContent = playlist.name
-
-        // listEl.appendChild(labelEl)
+        const labelEl = document.createElement('label')
+        labelEl.appendChild(checkBox)
+        labelEl.textContent = playlist.name
+        listEl.appendChild(labelEl)
         playlistsEl.appendChild(listEl)
     })
 }
